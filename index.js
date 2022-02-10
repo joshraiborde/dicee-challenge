392fc20a6bc8f8e7269f7d0228b6d35b2503d222
joshraiborde <joshraiborde@gmail.com>
2022-02-10 11:53:10 -0500

    prettier

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var randomNumber1 = Math.floor(Math.random()*6) + 1;
-var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var images1 = "images/dice" + randomNumber1 + ".png";
 var images2 = "images/dice" + randomNumber2 + ".png";
@@ -8,9 +8,9 @@ document.querySelector(".dice .img1").setAttribute("src", images1);
 document.querySelector(".dice .img2").setAttribute("src", images2);
 
 if (randomNumber1 === randomNumber2) {
-    document.querySelector("h1").innerHTML = "Draw! Click To Refresh";
+  document.querySelector("h1").innerHTML = "Draw! Click To Refresh";
 } else if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 Wins! Click To Refresh";
+  document.querySelector("h1").innerHTML = "Player 1 Wins! Click To Refresh";
 } else {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! Click To Refresh";
-       }
+  document.querySelector("h1").innerHTML = "Player 2 Wins! Click To Refresh";
+}
